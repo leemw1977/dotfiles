cb7629ea1bc33253201d47284473eba5b2d34390
Lee Williams <leewilliams@TCB.DIRECTORY>
2018-12-14 11:20:50 +0000

    add zsh and powershell files.

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,78 @@
+﻿
+# Ensure that Get-ChildItemColor is loaded
+Import-Module Get-ChildItemColor
+
+# Set l and ls alias to use the new Get-ChildItemColor cmdlets
+Set-Alias l Get-ChildItemColor -Option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
+
+# Helper function to change directory to my development workspace
+# Change c:\ws to your usual workspace and everytime you type
+# in git from PowerShell it will take you directly there.
+
+function go { Set-Location c:\git\ }
+# Obviously further functions can be added here to 
+# allow me to shortcut to the projects I work on most often.
+
+# Helper function to set location to the User Profile directory
+function cuserprofile { Set-Location ~ }
+Set-Alias ~ cuserprofile -Option AllScope
+
+# Helper function to show Unicode character
+function U
+{
+    param
+    (
+        [int] $Code
+    )
+ 
+    if ((0 -le $Code) -and ($Code -le 0xFFFF))
+    {
+        return [char] $Code
+    }
+ 
+    if ((0x10000 -le $Code) -and ($Code -le 0x10FFFF))
+    {
+        return [char]::ConvertFromUtf32($Code)
+    }
+ 
+    throw "Invalid character code $Code"
+}
+
+# open vi!
+$wslBasePath = "/mnt/c"
+function vi($dirAsStr) {
+    $dirAsStr = split-path -NoQualifier $dirAsStr
+
+    # My root drive is actually mounted
+    # in WSL as "/mnt/c" so if my path
+    # starts with "\" I need to append that
+    # you can start bash and type mount
+    # to find if yours is different.
+    
+    if ($dirAsStr.StartsWith("\")) {
+        $dirAsStr = Join-Path $wslBasePath $dirAsStr
+    }
+
+    $dirAsStr = $dirAsStr -replace "\\","/"
+
+    bash -c "vi $dirAsStr"
+}
+
+# Ensure posh-git is loaded
+Import-Module -Name posh-git
+
+# Start SshAgent if not already
+#setx GIT_SSH "C:\Windows\System32\OpenSSH\ssh.exe"
+# Need this if you are using github as your remote git repository
+#if (! (ps | ? { $_.Name -eq 'ssh-agent'})) {
+#    #Start-SshAgent
+#    C:\Windows\System32\OpenSSH\ssh-agent.exe
+#}
+
+# Ensure oh-my-posh is loaded
+Import-Module -Name oh-my-posh
+
+# Default the prompt to agnoster oh-my-posh theme
+#Set-Theme agnoster
+Set-Theme alteredbeast
