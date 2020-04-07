da0b9d9384f59a287d31253f63dd2b6646c436f0
Lee Williams <leemw1977@outlook.com>
2020-04-07 22:47:28 +0100

    Make package simpler

--- boxstarter/Leemw1977Installer/tools/ChocolateyInstall.ps1
+++ boxstarter/Leemw1977Installer/tools/ChocolateyInstall.ps1
@@ -1,70 +1,64 @@
 try {
-  # Set-ExecutionPolicy Bypass -Scope:Process
-  # Import-Module Boxstarter.Chocolatey
-  # Chocolatey pacakges:
-  # Install Vagrant/VirtBox/Ubuntu
-  cinst virtualbox -y
-  cinst vagrant -y
+    # Set-ExecutionPolicy Bypass -Scope:Process
+    # Import-Module Boxstarter.Chocolatey
+    # Chocolatey pacakges:
 
-  # CLI's
-  cinst powershell-core -y
-  cinst microsoft-windows-terminal -y
+    # CLI's
+    cinst powershell-core -y
+    cinst microsoft-windows-terminal -y
 
-  # Everyday software
-  cinst microsoft-edge -y
-  cinst 7zip
+    # Everyday software
+    cinst microsoft-edge -y
+    cinst 7zip
 
-  # Editors and IDE
-  cinst vscode -y
-  cinst jetbrains-rider -y
-  cinst notepadplusplus -y
+    # Editors and IDE
+    cinst vscode -y
+    cinst jetbrains-rider -y
+    cinst notepadplusplus -y
+    cinst hub -y
 
+    # Source control
+    cinst poshgit -y
+    cinst git -y
 
-  # Source control
-  cinst poshgit -y
-  cinst git -y
-  #TODO put this in - it's a bit flaky though if this is already installed it bombs boxstarter for some reason
-  #cinst gitkraken - y
+    # Copy setup of dotfiles to a local folder
+    $installFolder = 'leemw1977-dot-files'
+    $fullInstallPath = Join-Path -Path $env:LOCALAPPDATA -ChildPath $installFolder
+    $pathExists = Test-Path -Path $fullInstallPath
+    $sourcePath = Join-Path -Path (Get-PackageRoot($MyInvocation)) -ChildPath 'required_files'
+    if ($pathExists -eq $False) {
+        New-Item -Path $fullInstallPath -ItemType Directory
 
-#TODO Files aren't being included / found when we get to this stage
+        Write-Host "Copying from $sourcePath"
+        Copy-Item (Join-Path -Path ($sourcePath) -ChildPath '.tmux.conf') -Force (Join-Path -Path $fullInstallPath  -ChildPath '.tmux.conf')
+        Copy-Item (Join-Path -Path ($sourcePath) -ChildPath '.vimrc') -Force (Join-Path -Path $fullInstallPath  -ChildPath '.vimrc')
+        Copy-Item (Join-Path -Path ($sourcePath) -ChildPath '.zshrc') -Force (Join-PatH -Path $fullInstallPath  -ChildPath '.zshrc')
+        Copy-Item (Join-Path -Path ($sourcePath) -ChildPath 'powershell_profile.ps1') -Force (Join-Path -Path $fullInstallPath  -ChildPath 'powershell_profile.ps1')
+        Copy-Item (Join-Path -Path ($sourcePath) -ChildPath 'Cascadia.ttf') -Force (Join-Path -Path $fullInstallPath  -ChildPath 'Cascadia.ttf')
+        Copy-Item (Join-Path -Path ($sourcePath) -ChildPath 'CascadiaPL.ttf') -Force (Join-Path -Path $fullInstallPath  -ChildPath 'CascadiaPL.ttf')
+        Copy-Item (Join-Path -Path ($sourcePath) -ChildPath 'Hack-Bold.ttf') -Force (Join-Path -Path $fullInstallPath  -ChildPath 'Hack-Bold.ttf')
+        Copy-Item (Join-Path -Path ($sourcePath) -ChildPath 'Hack-BoldItalic.ttf') -Force (Join-Path -Path $fullInstallPath  -ChildPath 'Hack-BoldItalic.ttf')
+        Copy-Item (Join-Path -Path ($sourcePath) -ChildPath 'Hack-Regular.ttf') -Force (Join-Path -Path $fullInstallPath  -ChildPath 'Hack-Regular.ttf')
+    } else {
+      Write-BoxstarterMessage("dot files already exist, remove the directory $fullInstallPath if you want to reinstall from the package.")
+    }
 
-  # Copy setup of dotfiles to "temp folder"
-  $installFolder = 'leemw1977-dot-files'
-  $fullInstallPath = Join-Path -Path $env:LOCALAPPDATA -ChildPath $installFolder
-  $pathExists = Test-Path -Path $fullInstallPath
-
-  if ($pathExists -eq $False) {
-      New-Item -Path $fullInstallPath -ItemType Directory
-  } else {
-    Remove-Item -Recurse $fullInstallPath
-    New-Item -Path $fullInstallPath -ItemType Directory
-  }
-  Write-Host "Copying from $(Join-Path -Path (Get-PackageRoot($MyInvocation)) -ChildPath 'install.sh')"
-  Copy-Item (Join-Path -Path (Get-PackageRoot($MyInvocation)) -ChildPath 'install.sh') -Force (Join-Path -Path $fullInstallPath  -ChildPath 'install.sh')
-  Copy-Item (Join-Path -Path (Get-PackageRoot($MyInvocation)) -ChildPath '.tmux.conf') -Force (Join-Path -Path $fullInstallPath  -ChildPath '.tmux.conf')
-  Copy-Item (Join-Path -Path (Get-PackageRoot($MyInvocation)) -ChildPath '.vimrc') -Force (Join-Path -Path $fullInstallPath  -ChildPath '.vimrc')
-  Copy-Item (Join-Path -Path (Get-PackageRoot($MyInvocation)) -ChildPath '.zshrc') -Force (Join-Path -Path $fullInstallPath  -ChildPath '.zshrc')
-  Copy-Item (Join-Path -Path (Get-PackageRoot($MyInvocation)) -ChildPath '*.sh') -Force (Join-Path -Path $fullInstallPath  -ChildPath '*.sh')
-  Copy-Item (Join-Path -Path (Get-PackageRoot($MyInvocation)) -ChildPath '*.ttf') -Force (Join-Path -Path $fullInstallPath  -ChildPath '*.ttf')
-
-  Install-WindowsUpdate -Full -AcceptEula
+    Install-WindowsUpdate -Full -AcceptEula
 
 
-  # Set Windows features:
-  Write-BoxstarterMessage "Setting windows options"
-  Set-WindowsExplorerOptions -EnableShowFileExtensions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles 
-  Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-  Enable-MicrosoftUpdate
-  Enable-RemoteDesktop
+    # Set Windows features:
+    Write-BoxstarterMessage "Setting windows options"
+    Set-WindowsExplorerOptions -EnableShowFileExtensions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles 
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+    Enable-MicrosoftUpdate
+    Enable-RemoteDesktop
 
-  Write-BoxstarterMessage "Installing WSL"
-  Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile Ubuntu.appx -UseBasicParsing
-  Add-AppxPackage .\Ubuntu.appx
+    Write-BoxstarterMessage "Installing WSL"
+    Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile Ubuntu.appx -UseBasicParsing
+    Add-AppxPackage .\Ubuntu.appx
 
-
-  Write-ChocolateySuccess 'Leemw1977Installer'
-}
-catch {
-  Write-ChocolateyFailure 'Leemw1977Installer' $($_.Exception.Message)
+    Write-BoxstarterMessage 'Leemw1977Installer installed successfully'
+} catch {
+  Write-BoxstarterMessage 'Leemw1977Installer not installed successfully' $($_.Exception.Message)
   throw
 }
