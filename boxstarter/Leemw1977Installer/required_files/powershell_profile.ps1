29cab3e85ed309516f24e0111219d3944d9e05f0
Lee Williams <leemw1977@outlook.com>
2020-05-13 17:45:55 +0100

    ensure thefork command is registered in powershell

--- boxstarter/Leemw1977Installer/required_files/powershell_profile.ps1
+++ boxstarter/Leemw1977Installer/required_files/powershell_profile.ps1
@@ -63,3 +63,18 @@ function vim($dirAsStr) {
     # Pass to vi function
     vi($dirAsStr)
 }
+
+$env:PYTHONIOENCODING="utf-8"
+# fuck
+function Invoke-TheFork {
+    $history = (Get-History -Count 1).CommandLine;
+    if (-not [string]::IsNullOrWhiteSpace($history)) {
+        $fuck = $(thefuck $args $history);
+        if (-not [string]::IsNullOrWhiteSpace($fuck)) {
+            if ($fuck.StartsWith("echo")) { $fuck = $fuck.Substring(5); }
+            else { Invoke-Expression "$fuck"; }
+        }
+    }
+}
+
+Set-Alias -name "fork" -value "Invoke-TheFork"
